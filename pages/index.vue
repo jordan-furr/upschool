5787bfae76cabeb85cb91a8f6b5fba4495978423
jordanfurr <ejfurr@umich.edu>
2024-10-24 00:01:16 +0200

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,58 @@
 <template>
     <div>
         <div class="flex flex-row w-100">
-            <div class="w-55">
-
+            <div class="w-50 flex flex-col ph4 borderbox">
+                <div class="landing">
+                    <p>Initiatives based on helping people live rewarding lives.</p>
+                </div>
+                <div>
+                    <p class="nav-text">PROJECTS</p>
+                </div>
+                <div class="projects-home">
+                    <div class="project-item">
+                        <div class="project-title">
+                            <span>The Upside of Uncertainty</span>
+                            <span class="plus-icon">+</span>
+                        </div>
+                        <div class="project-content">
+                            <p>This is some hidden content related to 'The Upside of Uncertainty'.</p>
+                        </div>
+                    </div>
+                    <div class="project-item">
+                        <div class="project-title">
+                            <span>Earnest Project</span>
+                            <span class="plus-icon">+</span>
+                        </div>
+                        <div class="project-content">
+                            <p>This is some hidden content related to 'The Upside of Uncertainty'.</p>
+                        </div>
+                    </div>
+                    <div class="project-item">
+                        <div class="project-title">
+                            <span>Affection Economy</span>
+                            <span class="plus-icon">+</span>
+                        </div>
+                        <div class="project-content">
+                            <p>This is some hidden content related to 'The Upside of Uncertainty'.</p>
+                        </div>
+                    </div>
+                    <div class="project-item">
+                        <div class="project-title">
+                            <span>Hope Accelerator</span>
+                            <span class="plus-icon">+</span>
+                        </div>
+                        <div class="project-content">
+                            <p>This is some hidden content related to 'The Upside of Uncertainty'.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="w-45 img-half">
+            <div class="w-50 img-half">
 
             </div>
         </div>
-        <div class="info-button-cont">
-            <p class="info-button">INFO</p>
+        <div class="info-button-cont pl4">
+            <p class="nav-text blue-hover">INFO</p>
         </div>
     </div>
 </template>
