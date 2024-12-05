8c8cea5fd60bc2ca8abf17ca00d3e56cd91d35f9
jordan-furr <jordanfurr98@gmail.com>
2024-12-05 10:51:53 +0100

    info page mobile

--- pages/info.vue
+++ pages/info.vue
@@ -1,15 +1,37 @@
 <template>
     <div class="h-100">
         <info-header />
-        <div class="info-page pl4 pr6 flex-col">
-            <div class="flex-row pb6 borderbox">
-                <div class="w-25">
-                    <div class="w-75">
-                        <p class="info-sub">ABOUT</p>
+        <div class="info-page ph4 flex-col">
+            <div class="flex-row pb6 borderbox info-row">
+                <div class="w-25 mobile-100 pb5 flex-row ">
+                    <div class="w-75 mobile-50">
                         <p>Research and education initiative based online and in Paris, France.</p>
                     </div>
+
+                    <div class="w-25 desktop-hide mobile-50 text-right">
+                        <div>
+                            <a href="mailto:info@upschool.org">
+                                <p><span class="info-link">info@upschool.org</span></p>
+                            </a>
+
+                        </div>
+                    </div>
                 </div>
-                <div class="w-25">
+                <div class="w-25 mobile-50 pb5">
+                    <div class="w-100">
+                        <p>Led by Susannah HARMON FURR, art historian and entrepreneur, and Nathan FURR, professor of
+                            strategy at INSEAD.</p>
+
+                    </div>
+                </div>
+                <div class="w-50 mobile-100 pb3 mobile-hide">
+                    <div class="w-60 float-right">
+                        <p>So, friends, every day do something that won’t compute. Wendell Berry.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-row">
+                <div class="w-25 mobile-50">
                     <div>
                         <p class="info-sub">PROJECTS</p>
                         <nuxt-link to="/upside-of-uncertainty" exact-active-class="active">
@@ -29,23 +51,7 @@
                         </nuxt-link>
                     </div>
                 </div>
-                <div class="w-50">
-                    <div class="w-60 float-right">
-                        <p>So, friends, every day do something that won’t compute. Wendell Berry.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex-row">
-                <div class="w-25">
-                    <div class="w-75">
-                        <p>UP School is led by Nathan FURR, professor of strategy at INSEAD, and Susannah HARMON
-                            FURR,
-                            art
-                            historian and designer.</p>
-
-                    </div>
-                </div>
-                <div class="w-25">
+                <div class="w-25 mobile-hide">
                     <div>
                         <a href="mailto:info@upschool.org">
                             <p><span class="info-link">info@upschool.org</span></p>
@@ -54,7 +60,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
         <div class="backpack-cont">
             <img class="backpack" src="/assets/UPschool-backpack.webp" alt="">
@@ -70,6 +75,11 @@
                         <line x1="6.97" y1="6.97" x2="40.91" y2="40.91" stroke="white" stroke-width="2" />
                     </svg>
                 </p>
+            </div>
+        </div>
+        <div class="subscribe-cont">
+            <div class="subscribe-box">
+                <p>subscribe</p>
             </div>
         </div>
     </div>
